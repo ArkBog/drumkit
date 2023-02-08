bdc1ab0eb54a469d3ac4b91802d9551814479af8
ArkBog <arkadiuszbogdanski1@gmail.com>
2023-02-08 20:07:10 +0100

    some changes

--- main.js
+++ main.js
@@ -6,8 +6,26 @@ container.classList.add("container");
 for (let i = 0; i < 9; i++) {
   let button = document.createElement("button");
   button.classList.add("button");
+  button.setAttribute("value", `${[i]}`);
 
   container.appendChild(button);
-}
+};
+
+function playAudio(){
+  
+    kick.play();
+};
+
+function myAlert(){
+  alert("działa")
+};
 
 app.appendChild(container);
+
+const pads = document.getElementsByClassName("button");
+console.log(pads);
+const kick = pads[0];
+
+console.log(kick);
+
+kick.addEventListener("click", playAudio);
