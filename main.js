7532e4c672fdf9daf5e6e41ad63259bb00705294
ArkBog <arkadiuszbogdanski1@gmail.com>
2023-02-12 19:06:50 +0100

    add a dsiplay and alert, create function for alert, style for mobile

--- main.js
+++ main.js
@@ -3,6 +3,34 @@ const app = document.querySelector("#app");
 const container = document.createElement("div");
 container.classList.add("container");
 
+
+
+const alertBlock = document.createElement("div");
+alertBlock.classList.add("alert");
+app.appendChild(alertBlock);
+
+const alertText = document.createElement("div");
+alertText.classList.add("alert-text");
+alertBlock.innerText = "Bringing the power of a full drum set to your fingertips, anytime, anywhere. Click or press key to play a sound.";
+alertBlock.appendChild(alertText);
+
+const alertClose = document.createElement("div");
+alertClose.classList.add("alert-close");
+alertClose.innerText = "X"
+alertBlock.appendChild(alertClose);
+
+function closeAlert(){
+  alertBlock.style.display = "none"
+};
+
+alertClose.addEventListener("click", closeAlert);
+
+
+const display = document.createElement("div");
+display.classList.add("display");
+container.appendChild(display);
+
+
 const pads = [{name: "clap", letter: "W"}, {name: "hihat", letter: "S"}, {name: "kick", letter: "D"}, {name: "openhat", letter: "F"}, {name: "boom", letter: "G"}, {name: "ride", letter: "H"}, {name: "snare", letter: "J"}, {name:"tom", letter: "K"}, {name: "tink", letter: "L"}];
 
 pads.forEach((pad) =>{
@@ -71,5 +99,6 @@ document.addEventListener("keydown", (e) => {
       break;
   }
 });
+
 
 app.appendChild(container);
